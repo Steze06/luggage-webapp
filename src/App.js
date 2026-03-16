df253eff98f1c92e8acdd59fe685a42ea076b546
Stezeul <caradeanu.stefan@gmail.com>
2026-03-16 20:20:20 +0200

    footer update

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ export default function App() {
       />
       {/*functia handleAddItems si items au fost transmise ca props pentru 
       Form si PackingList */}
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -56,7 +56,7 @@ function Form({ onAddItems }) {
     if (!description) return;
 
     const newItem = { description, quantity, packed: false, id: Date.now() };
-    console.log(newItem);
+    // console.log(newItem);
 
     onAddItems(newItem);
 
@@ -123,12 +123,26 @@ function Item({ item, onDeleteItem, onToggleItems }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding items to your packing list 🚀</em>
+      </p>
+    );
+
   const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
 
   return (
     <footer className="stats">
-      <em>👜You have X items on your list, and you already packed X (X%)</em>
+      <em>
+        {percentage === 100
+          ? "You got everything! Ready to go ✈️"
+          : `👜You have ${numItems} items on your list, and you already packed
+        ${numPacked} (${percentage}%)`}
+      </em>
     </footer>
   );
 }
